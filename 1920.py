fe6ed5a3a2dd842ba543ff3aeb69a01dd80fa084
ohyujeong <ohlife4@gmail.com>
2021-07-09 09:39:38 +0900

    수 찾기

---
+++ 1920.py
@@ -0,0 +1,11 @@
+N=int(input())
+A=set(map(int,input().split()))
+
+M=int(input())
+B=list(map(int,input().split()))
+
+for i in range(M):
+    if B[i] in A:
+        print(1)
+    else:
+        print(0)
