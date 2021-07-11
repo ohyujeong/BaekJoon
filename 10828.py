31a506472d0a9232147fb9f4b6ce466b8dd1a506
ohyujeong <ohlife4@gmail.com>
2021-07-11 15:10:21 +0900

    스택

---
+++ 10828.py
@@ -0,0 +1,31 @@
+import sys
+
+stack=[]
+
+N=int(sys.stdin.readline())
+
+for i in range(N):
+    command=sys.stdin.readline().strip().split()
+    if command[0]=='push':
+        num=int(command[1])
+        stack.append(num)
+    elif command[0]=='pop':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
+            stack.pop()
+    elif command[0]=='size':
+        print(len(stack))
+    elif command[0]=='empty':
+        if not stack:
+            print(1)
+        else:
+            print(0)
+    elif command[0]=='top':
+        if not stack:
+            print(-1)
+        else:
+            print(stack[-1])
+
+
